8a8600d092120a4a87fe8f9ea6c01e3e9d54b322
SylvinIsamaza <isamazasylvin@gmail.com>
2023-11-11 14:48:41 +0200

    Updating socket file

--- src/socket.js
+++ src/socket.js
@@ -3,4 +3,4 @@ import { io } from 'socket.io-client';
 // "undefined" means the URL will be computed from the `window.location` object
 const URL = 'https://scraper-backend-yg6r.onrender.com';
 
-export const socket = io(URL);
+export const socket = io(URL, {transports: ['websocket']});
