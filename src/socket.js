3a9fa563957e210e0e96cb2b8c71856fce9ae50c
SylvinIsamaza <isamazasylvin@gmail.com>
2023-11-11 13:31:09 +0200

    Changing the backend url

--- src/socket.js
+++ src/socket.js
@@ -1,6 +1,6 @@
 import { io } from 'socket.io-client';
 
 // "undefined" means the URL will be computed from the `window.location` object
-const URL = 'http://localhost:5000';
+const URL = 'https://scraper-backend-yg6r.onrender.com';
 
 export const socket = io(URL);
