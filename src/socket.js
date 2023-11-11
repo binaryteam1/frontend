989c1c3ed05c389bbaf68378017248ef2bd90b00
SylvinIsamaza <isamazasylvin@gmail.com>
2023-11-11 15:56:19 +0200

    Updating backend

--- src/socket.js
+++ src/socket.js
@@ -1,6 +1,6 @@
 import { io } from 'socket.io-client';
 
 // "undefined" means the URL will be computed from the `window.location` object
-const URL = 'scraper-backend-yg6r.onrender.com';
+const URL = 'https://scraper-backend-yg6r.onrender.com';
 
-export const socket = io(URL, {transports: ['websocket']});
+export const socket = io(URL);
