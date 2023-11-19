7e7f42d9e800ae3cdf388598db8cbc5a6504d317
SylvinIsamaza <isamazasylvin@gmail.com>
2023-11-19 22:43:10 +0200

    Updating frontend

--- src/socket.js
+++ src/socket.js
@@ -3,7 +3,7 @@ import { io } from "socket.io-client";
 // "undefined" means the URL will be computed from the `window.location` object
 const URL =
   process.env.NODE_ENV === "production"
-    ? "http://13.234.215.187:5000"
-    : "http://13.234.215.187:5000";
+    ? "https://scraper-backend-yg6r.onrender.com"
+    : "https://scraper-backend-yg6r.onrender.com";
 
 export const socket = io(URL);
