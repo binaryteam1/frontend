e46ead2d36be91ff6b80b07d2d64cd20a0b22863
SylvinIsamaza <isamazasylvin@gmail.com>
2023-11-20 17:33:52 +0200

    Updating socket

--- src/components/Category.jsx
+++ src/components/Category.jsx
@@ -17,56 +17,38 @@ function Category() {
   useEffect(() => {
     const handleSocketData = (data) => {
       setTestData((prevTestData) => {
-        // Ensure data is an array
         const newDataArray = Array.isArray(data) ? data : [data];
-
-        const isNewDataPresent = newDataArray.some(
-          (newData) =>
-            prevTestData &&
-            prevTestData.some(
-              (item) =>
-                item.markets &&
-                item.markets[0] &&
-                newData.markets &&
-                newData.markets[0] &&
-                parseFloat(item.markets[0].marketId) === parseFloat(newData.markets[0].marketId) &&
-                parseFloat(item.markets[0].eventId) === parseFloat(newData.markets[0].eventId) &&
-                parseFloat(item.markets[0].eventType) === parseFloat(newData.markets[0].eventType)
-            )
-        );
-
-        const updatedTestData = isNewDataPresent
-          ? prevTestData
-          : [...prevTestData, ...newDataArray]; // Using spread operator to flatten the array
-
-        setFilteredData(data);
-
-        return data;
-        // return updatedTestData;
+        const updatedTestData = [...prevTestData, ...newDataArray];
+        return updatedTestData;
       });
     }
 
     socket.on('1', handleSocketData);
     socket.on('2', handleSocketData);
     socket.on('3', handleSocketData);
-    const filtered = () => {
-      if (testData.length === 0) return [];
 
-      return testData.filter((event) => {
-        const market = event.markets.length > 0 && event.markets[0];
-
-        return (
-          (!eventId || (market && parseFloat(market.eventId) === parseFloat(eventId))) &&
-          (!marketId || (market && parseFloat(market.marketId) === parseFloat(marketId))) &&
-          (!eventCategory || (market && parseFloat(market.eventType) === parseFloat(eventCategory)))
-        );
-      });
-    };
-
-    setFilteredData(filtered());
     return () => {
+
     };
-  }, [eventId, marketId,testData]);
+  }, []);
+
+  useEffect(() => {
+    setFilteredData(filtered());
+  }, [testData, eventId, marketId, eventCategory]);
+
+  const filtered = () => {
+    if (testData.length === 0) return [];
+
+    return testData.filter((event) => {
+      const market = event.markets.length > 0 && event.markets[0];
+
+      return (
+        (!eventId || (market && parseFloat(market.eventId) === parseFloat(eventId))) &&
+        (!marketId || (market && parseFloat(market.marketId) === parseFloat(marketId))) &&
+        (!eventCategory || (market && parseFloat(market.eventType) === parseFloat(eventCategory)))
+      );
+    });
+  };
 
   return (
     <div>
