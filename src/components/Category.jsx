bc29bb291ea514b9ee4d8aa89a9157e902549e7a
SylvinIsamaza <isamazasylvin@gmail.com>
2023-11-20 20:17:07 +0200

    replacing instead of appending

--- src/components/Category.jsx
+++ src/components/Category.jsx
@@ -28,10 +28,7 @@ function Category() {
     socket.on('3', handleSocketData);
 
     return () => {
-      // Cleanup socket listeners when component unmounts
-      socket.off('1', handleSocketData);
-      socket.off('2', handleSocketData);
-      socket.off('3', handleSocketData);
+
     };
   }, []);
 
