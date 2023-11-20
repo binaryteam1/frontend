f3231898489d7dd260a9ce02e33b472b09928c3d
SylvinIsamaza <isamazasylvin@gmail.com>
2023-11-21 00:43:50 +0200

    Updating socket

--- src/redux/reducers/dataslice.js
+++ src/redux/reducers/dataslice.js
@@ -1,73 +1,18 @@
-// Category.js
-import React, { useEffect, useState } from "react";
-import { useLocation } from "react-router-dom";
-import { useDispatch, useSelector } from 'react-redux';
-import { socket } from "../socket";
-import { addSocketData } from "../dataSlice"; // Import the Redux action
+// dataSlice.js
+import { createSlice } from '@reduxjs/toolkit';
 
-function Category() {
-  const location = useLocation();
-  const dispatch = useDispatch();
-  const testData = useSelector((state) => state.data.testData);
+export const dataSlice = createSlice({
+  name: 'data',
+  initialState: {
+    testData: [],
+  },
+  reducers: {
+    addSocketData: (state, action) => {
+      state.testData = [...state.testData, ...action.payload];
+    },
+  },
+});
 
-  const [filteredData, setFilteredData] = useState([]);
+export const { addSocketData } = dataSlice.actions;
 
-  const queryParams = new URLSearchParams(location.search);
-  const queryData = Object.fromEntries(queryParams);
-
-  const eventId = queryData["eventId"];
-  const marketId = queryData["marketId"];
-  const eventCategory = queryData['eventType'];
-
-  const filtered = () => {
-    return testData.filter((event) => {
-      const market = event.markets.length > 0 && event.markets[0];
-
-      return (
-        (!eventId || (market && parseFloat(market.eventId) === parseFloat(eventId))) &&
-        (!marketId || (market && parseFloat(market.marketId) === parseFloat(marketId))) &&
-        (!eventCategory || (market && parseFloat(market.eventType) === parseFloat(eventCategory)))
-      );
-    });
-  };
-
-  useEffect(() => {
-    const handleSocketData = (data) => {
-      // Dispatch the action to update the Redux store
-      dispatch(addSocketData(Array.isArray(data) ? data : [data]));
-    };
-
-    if (eventCategory) {
-      socket.on(eventCategory.toString(), handleSocketData);
-    } else {
-      socket.on('1', handleSocketData);
-      socket.on('2', handleSocketData);
-      socket.on('3', handleSocketData);
-    }
-
-    return () => {
-      // Cleanup socket listeners when component unmounts
-      if (eventCategory) {
-        socket.off(eventCategory.toString(), handleSocketData);
-      }
-    };
-  }, [eventId, marketId, eventCategory, dispatch]);
-
-  useEffect(() => {
-    const filterData = filtered();
-    setFilteredData(filterData);
-  }, [testData, eventId, marketId, eventCategory]);
-
-  return (
-    <div>
-      <div>Category</div>
-      <div>
-        <div>{JSON.stringify(filteredData)}</div>
-        {/* Display or use the eventData as needed */}
-        <p>Data received from the server:</p>
-      </div>
-    </div>
-  );
-}
-
-export default Category;
+export default dataSlice.reducer;
