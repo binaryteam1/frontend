70d50d1b946cc269f4dd404aa2a341c47fb24270
SylvinIsamaza <isamazasylvin@gmail.com>
2023-11-21 01:21:43 +0200

    Updating socket

--- src/components/Category.jsx
+++ src/components/Category.jsx
@@ -1,64 +1,67 @@
-  import React, { useEffect, useState } from "react";
-  import { useLocation } from "react-router-dom";
-  import { socket } from "../socket"; // Import the socket instance
+import React, { useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
+import { socket } from "../socket"; // Import the socket instance
 
-  function Category() {
-    const location = useLocation();
-    const [testData, setTestData] = useState([]);
-    const [filteredData, setFilteredData] = useState([]);
+function Category() {
+  const location = useLocation();
+  const [testData, setTestData] = useState([]);
+  const [filteredData, setFilteredData] = useState([]);
 
-    const queryParams = new URLSearchParams(location.search);
-    const queryData = Object.fromEntries(queryParams);
+  const queryParams = new URLSearchParams(location.search);
+  const queryData = Object.fromEntries(queryParams);
 
-    const eventId = queryData["eventId"];
-    const marketId = queryData["marketId"];
-    const eventCategory = queryData['eventType'];
-    const filtered = () => {
-      return testData.filter((event) => {
-        const market = event.markets.length > 0 && event.markets[0];
+  const eventId = queryData["eventId"];
+  const marketId = queryData["marketId"];
+  const eventCategory = queryData['eventType'];
+  const filtered = () => {
+    return testData.filter((event) => {
+      const market = event.markets.length > 0 && event.markets[0];
 
-        return (
-          (!eventId || (market && parseFloat(market.eventId) === parseFloat(eventId))) &&
-          (!marketId || (market && parseFloat(market.marketId) === parseFloat(marketId))) &&
-          (!eventCategory || (market && parseFloat(market.eventType) === parseFloat(eventCategory)))
-        );
-      });
-    };
-    const handleSocketData = (data) => {
-      setTestData((prevTestData) => {
-        const newDataArray = Array.isArray(data) ? data : [data];
-        const updatedTestData = [...prevTestData, ...newDataArray];
-        // return updatedTestData;
-        return data;
-      });
-    }
-    socket.on('1', handleSocketData);
-    socket.on('2', handleSocketData);
-    socket.on('3', handleSocketData);
-    useEffect(() => {
+      return (
+        (!eventId || (market && parseFloat(market.eventId) === parseFloat(eventId))) &&
+        (!marketId || (market && parseFloat(market.marketId) === parseFloat(marketId))) &&
+        (!eventCategory || (market && parseFloat(market.eventType) === parseFloat(eventCategory)))
+      );
+    });
+  };
 
-      const filterData = filtered();
-      setFilteredData(filterData);
-     
-
-      return () => {
-
-      };
-    }, [filteredData,eventId, marketId, eventCategory]);
-
-
-
-
-    return (
-      <div>
-        <div>Category</div>
-        <div>
-          <div>{JSON.stringify(filteredData)}</div>
-          {/* Display or use the eventData as needed */}
-          <p>Data received from the server:</p>
-        </div>
-      </div>
-    );
+  const handleSocketData = (data) => {
+    setTestData((prevTestData) => {
+      const newDataArray = Array.isArray(data) ? data : [data];
+      const updatedTestData = [...prevTestData, ...newDataArray];
+      // return updatedTestData;
+      return data;
+    });
   }
+  
+  socket.on('1', handleSocketData);
+  socket.on('2', handleSocketData);
+  socket.on('3', handleSocketData);
+  useEffect(() => {
+    const filterData = filtered();
+    setFilteredData(filterData);
 
-  export default Category;
+    return () => {
+      // Cleanup socket listeners when component unmounts
+      socket.off('1', handleSocketData);
+      socket.off('2', handleSocketData);
+      socket.off('3', handleSocketData);
+    };
+  }, [testData,eventId, marketId, eventCategory]);
+
+
+
+
+  return (
+    <div>
+      <div>Category</div>
+      <div>
+        <div>{JSON.stringify(filteredData)}</div>
+        {/* Display or use the eventData as needed */}
+        <p>Data received from the server:</p>
+      </div>
+    </div>
+  );
+}
+
+export default Category;
