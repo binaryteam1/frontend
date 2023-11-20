fc7e582a525f553e5075aa60fef92ea09757e0ae
SylvinIsamaza <isamazasylvin@gmail.com>
2023-11-20 17:52:55 +0200

    Updating socket

--- src/components/Category.jsx
+++ src/components/Category.jsx
@@ -1,77 +1,77 @@
-import React, { useEffect, useState } from "react";
-import { useLocation } from "react-router-dom";
-import { socket } from "../socket"; // Import the socket instance
+  import React, { useEffect, useState } from "react";
+  import { useLocation } from "react-router-dom";
+  import { socket } from "../socket"; // Import the socket instance
 
-function Category() {
-  const location = useLocation();
-  const [testData, setTestData] = useState([]);
-  const [filteredData, setFilteredData] = useState([]);
+  function Category() {
+    const location = useLocation();
+    const [testData, setTestData] = useState([]);
+    const [filteredData, setFilteredData] = useState([]);
 
-  const queryParams = new URLSearchParams(location.search);
-  const queryData = Object.fromEntries(queryParams);
+    const queryParams = new URLSearchParams(location.search);
+    const queryData = Object.fromEntries(queryParams);
 
-  const eventId = queryData["eventId"];
-  const marketId = queryData["marketId"];
-  const eventCategory = queryData['eventType'];
+    const eventId = queryData["eventId"];
+    const marketId = queryData["marketId"];
+    const eventCategory = queryData['eventType'];
 
-  useEffect(() => {
-    const handleSocketData = (data) => {
-      setTestData((prevTestData) => {
-        // const newDataArray = Array.isArray(data) ? data : [data];
-        // const updatedTestData = [...prevTestData, ...newDataArray];
-        // return updatedTestData;
-        return data;
-      });
-    }
-    if (eventCategory) {
-      socket.on(eventCategory, handleSocketData);
-   
-
-    }
-    else if (eventCategory == 4) {
-      eventCategory = 3;
-      socket.on(toString(eventCategory),handleSocketData)
-    }
-    else {
-      socket.on('1',handleSocketData)
-      socket.on('2', handleSocketData);
-      socket.on('3', handleSocketData);
-    }
-
+    useEffect(() => {
+      const handleSocketData = (data) => {
+        setTestData((prevTestData) => {
+          // const newDataArray = Array.isArray(data) ? data : [data];
+          // const updatedTestData = [...prevTestData, ...newDataArray];
+          // return updatedTestData;
+          return data;
+        });
+      }
+      if (eventCategory) {
+        socket.on(eventCategory, handleSocketData);
     
-    return () => {
-      socket.off(!eventCategory, handleSocketData);
+
+      }
+      else if (eventCategory == 4) {
+        eventCategory = 3;
+        socket.on(toString(eventCategory),handleSocketData)
+      }
+      else {
+        socket.on('1',handleSocketData)
+        socket.on('2', handleSocketData);
+        socket.on('3', handleSocketData);
+      }
+
+      
+      return () => {
+      
+      };
+    }, [eventId,marketId,eventCategory]);
+
+    useEffect(() => {
+      setFilteredData(filtered());
+    }, [testData, eventId, marketId, eventCategory]);
+
+    const filtered = () => {
+      if (testData.length === 0) return [];
+
+      return testData.filter((event) => {
+        const market = event.markets.length > 0 && event.markets[0];
+
+        return (
+        marketId&&eventId&&eventCategory?(!eventId || (market && parseFloat(market.eventId) === parseFloat(eventId))) &&
+          (!marketId || (market && parseFloat(market.marketId) === parseFloat(marketId))) &&
+          (!eventCategory || (market && parseFloat(market.eventType) === parseFloat(eventCategory))):eventCategory&(!eventCategory || (market && parseFloat(market.eventType) === parseFloat(eventCategory)))
+        );
+      });
     };
-  }, [eventId,marketId,eventCategory]);
 
-  useEffect(() => {
-    setFilteredData(filtered());
-  }, [testData, eventId, marketId, eventCategory]);
-
-  const filtered = () => {
-    if (testData.length === 0) return [];
-
-    return testData.filter((event) => {
-      const market = event.markets.length > 0 && event.markets[0];
-
-      return (
-       marketId&&eventId&&eventCategory?(!eventId || (market && parseFloat(market.eventId) === parseFloat(eventId))) &&
-        (!marketId || (market && parseFloat(market.marketId) === parseFloat(marketId))) &&
-        (!eventCategory || (market && parseFloat(market.eventType) === parseFloat(eventCategory))):eventCategory&(!eventCategory || (market && parseFloat(market.eventType) === parseFloat(eventCategory)))
-      );
-    });
-  };
-
-  return (
-    <div>
-      <div>Category</div>
+    return (
       <div>
-        <div>{JSON.stringify(filteredData)}</div>
-        {/* Display or use the eventData as needed */}
-        <p>Data received from the server:</p>
+        <div>Category</div>
+        <div>
+          <div>{JSON.stringify(filteredData)}</div>
+          {/* Display or use the eventData as needed */}
+          <p>Data received from the server:</p>
+        </div>
       </div>
-    </div>
-  );
-}
+    );
+  }
 
-export default Category;
+  export default Category;
