d9794d41aab784ed7e9f4aa478ad369a0075cb3c
SylvinIsamaza <isamazasylvin@gmail.com>
2023-11-21 03:11:02 +0200

    Updating socket

---
+++ src/components/Single.jsx
@@ -0,0 +1,53 @@
+import React, { useEffect, useState, useMemo } from "react";
+import { useLocation } from "react-router-dom";
+import { socket } from "../socket"; // Import the socket instance
+
+function Category() {
+  const location = useLocation();
+  const [testData, setTestData] = useState([]);
+  const [filteredData, setFilteredData] = useState([]);
+
+  const queryParams = new URLSearchParams(location.search);
+  const queryData = Object.fromEntries(queryParams);
+
+  const eventId = queryData["eventId"];
+  const marketId = queryData["marketId"];
+  const eventCategory = queryData['eventType'];
+
+ 
+
+
+  
+
+  useEffect(() => {
+      
+  const handleSocketData = (data) => {
+    
+
+    setFilteredData(data.filter((event) => {
+       const market = event.markets.length > 0 && event.markets[0];
+ 
+       return (
+         (!eventId || (market && parseFloat(market.eventId) === parseFloat(eventId))) &&
+         (!marketId || (market && parseFloat(market.marketId) === parseFloat(marketId))) &&
+         (!eventCategory || (market && parseFloat(market.eventType) === parseFloat(eventCategory)))
+       );
+     }))
+   }
+   ;
+   socket.on(eventCategory.toString(), handleSocketData);
+  }, [setFilteredData]);
+
+  return (
+    <div>
+      <div>Category</div>
+      <div>
+        <div>{JSON.stringify(filteredData)}</div>
+        {/* Display or use the eventData as needed */}
+        <p>Data received from the server:</p>
+      </div>
+    </div>
+  );
+}
+
+export default Category;
